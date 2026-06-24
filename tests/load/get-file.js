edc07bc7105b75cbe9fceed561bb0d6491ede201
ChristophThiel <christoph.thiel@hotmail.com>
2026-06-24 15:50:40 +0200

    Setup load test

---
+++ tests/load/get-file.js
@@ -0,0 +1,14 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+
+export const options = {
+  vus: 1,
+  iterations: 10,
+  insecureSkipTLSVerify: true
+}
+
+export default function () {
+  let res = http.get(__ENV.SHARE_URL)
+  check(res, { 'status is 200': (res) => res.status === 200 })
+  sleep(1)
+}
---
+++ tests/load/scripts/prepare-get-file.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+BASE_URL="https://host.docker.internal:9200"
+USER="dennis"
+PASS="demo"
+
+curl -k -X PUT "$BASE_URL/dav/files/$USER/load-test.txt" -u "$USER:$PASS" --data-binary "Hello, World!"
+
+read FILE_ID DRIVE_ID < <(
+  curl -k "$BASE_URL/graph/v1.0/me/drive/root/children" -u "$USER:$PASS" \
+  | jq -r '.value[] 
+    | select(.name=="load-test.txt") 
+    | "\(.id) \(.parentReference.driveId)"'
+)
+
+echo $FILE_ID
+echo $DRIVE_ID
+
+SHARE_URL=$(curl -k -X POST -s \
+  "$BASE_URL/graph/v1beta1/drives/$DRIVE_ID/items/$FILE_ID/createLink" \
+  -u "$USER:$PASS" \
+  -H "Content-Type: application/json" \
+  -d '{"type":"view"}' \
+  | jq -r '.link.webUrl')
+
+echo $SHARE_URL
+echo "SHARE_URL=$SHARE_URL" >> $GITHUB_ENV
