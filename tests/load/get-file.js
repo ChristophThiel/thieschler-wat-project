01f39365187cf2210eea85eac34c829252a753ff
ChristophThiel <christoph.thiel@hotmail.com>
2026-06-24 16:04:35 +0000

    Use stages

--- tests/load/get-file.js
+++ tests/load/get-file.js
@@ -2,9 +2,12 @@ import http from 'k6/http'
 import { check, sleep } from 'k6'
 
 export const options = {
-  vus: 20,
-  duration: '30s',
-  insecureSkipTLSVerify: true
+  stages: [
+    { duration: '10s', target: 10 },
+    { duration: '20s', target: 50 },
+    { duration: '30s', target: 50 },
+    { duration: '10s', target: 0 }
+  ]
 }
 
 export default function () {
