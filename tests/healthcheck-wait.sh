169f20af64b81c3c4734ea853549d1c82c271170
ChristophThiel <christoph.thiel@hotmail.com>
2026-06-24 15:38:36 +0000

    Add health and run fix

--- tests/healthcheck-wait.sh
+++ tests/healthcheck-wait.sh
@@ -3,7 +3,8 @@ set -e
 
 for c in $(docker compose ps -q); do
   echo "Wait for $c..."
-  until [ "$(docker inspect -f '{{.State.Health.Status}}' $c)" = "healthy" ]; do
+  until [ "$(docker inspect -f '{{if .State.Health}}{{.State.Health.Status}}{{else}}{{.State.Status}}{{end}}' $c)" = "healthy" ] \
+     || [ "$(docker inspect -f '{{if .State.Health}}{{.State.Health.Status}}{{else}}{{.State.Status}}{{end}}' $c)" = "running" ]; do
     sleep 2
   done
 done
