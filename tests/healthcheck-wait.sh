9f4cd35e9bc841beabd1fa4c3e7cd2fd68cfd77f
ChristophThiel <christoph.thiel@hotmail.com>
2026-06-24 15:44:53 +0000

    Make health check only for opencloud container

--- tests/healthcheck-wait.sh
+++ tests/healthcheck-wait.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-for c in $(docker compose ps -q); do
-  echo "Wait for $c..."
-  until [ "$(docker inspect -f '{{if .State.Health}}{{.State.Health.Status}}{{else}}{{.State.Status}}{{end}}' $c)" = "healthy" ] \
-     || [ "$(docker inspect -f '{{if .State.Health}}{{.State.Health.Status}}{{else}}{{.State.Status}}{{end}}' $c)" = "running" ]; do
-    sleep 2
-  done
+until [ "$(docker inspect -f '{{.State.Health.Status}}' web_oc)" = "healthy" ]; do
+  sleep 2
 done
